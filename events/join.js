d36a8e9350c9a365c9fac7892d182b811358cbd0
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-06 18:13:32 +0500

    join.js

--- events/join.js
+++ events/join.js
@@ -1,1 +1,12 @@
+const config = require("../config");
 
+module.exports = {
+  name: "guildMemberAdd",
+  execute(member) {
+
+    const channel = member.guild.channels.cache.get(config.welcomeChannel);
+    if (!channel) return;
+
+    channel.send(`🎉 Welcome ${member} to the server!`);
+  }
+};
