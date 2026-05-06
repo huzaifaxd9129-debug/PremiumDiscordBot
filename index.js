f48c50646cb4cad3f27b26683fd01912edc53748
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-06 18:11:17 +0500

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+const { Client, GatewayIntentBits, Partials, Collection } = require("discord.js");
+const fs = require("fs");
+const config = require("./config");
 
+const client = new Client({
+  intents: Object.values(GatewayIntentBits),
+  partials: Object.values(Partials)
+});
+
+client.commands = new Collection();
+
+// LOAD COMMANDS
+const commandFiles = fs.readdirSync("./commands").filter(f => f.endsWith(".js"));
+for (const file of commandFiles) {
+  const cmd = require(`./commands/${file}`);
+  client.commands.set(cmd.name, cmd);
+}
+
+// LOAD EVENTS
+const eventFiles = fs.readdirSync("./events").filter(f => f.endsWith(".js"));
+for (const file of eventFiles) {
+  const event = require(`./events/${file}`);
+  client.on(event.name, (...args) => event.execute(...args, client));
+}
+
+client.once("ready", () => {
+  console.log(`✅ Logged in as ${client.user.tag}`);
+});
+
+client.login(config.token);
