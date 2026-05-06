0ba94da7a2ce462e8e6004fa217e37a4ee330890
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-06 18:08:21 +0500

    config.js

--- config.js
+++ config.js
@@ -1,1 +1,6 @@
-
+module.exports = {
+  token: process.env.TOKEN,
+  prefix: process.env.PREFIX || "+",
+  welcomeChannel: process.env.WELCOME_CHANNEL,
+  verifiedRole: process.env.VERIFIED_ROLE
+};
